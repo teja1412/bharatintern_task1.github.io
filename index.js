8e4644ccb81b1902d877698269a2debefcfaaeb4
teja1412 <95267030+teja1412@users.noreply.github.com>
2024-01-29 00:27:29 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+var express =require("express")
+var bodyparser=require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyparser.json())
+app.use(express.static('public'))
+app.use(bodyparser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/mydb',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db=mongoose.connection;
+
+db.on('error',()=>console.log("error in connecting to database"));
+db.once('open',()=>console.log("connected to database"))
+
+app.post("/sign_up",(req,res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var phno = req.body.phno;
+    var password = req.body.password;
+
+    var data={
+        "name": name,
+        "email": email,
+        "phno": phno,
+        "password": password
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Succesfully");
+    });
+
+    return res.redirect('signup_success.html')
+})
+
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('index.html');
+}).listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
