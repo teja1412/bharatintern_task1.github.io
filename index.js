848e26904d99e895bf787c2eabc0df0b5e1f1907
teja1412 <95267030+teja1412@users.noreply.github.com>
2024-01-29 07:34:32 +0530

    index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyparser.urlencoded({
     extended:true
 }))
 
-mongoose.connect('mongodb://localhost:27017/mydb',{
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/mydb',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
